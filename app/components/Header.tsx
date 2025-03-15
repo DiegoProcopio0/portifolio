9a84da064d8f62ba3dedd6d9e0f139249627681d
Diego Procopio <diegprocopio98@gmail.com>
2025-03-15 15:41:39 -0300

    corrige o nome do projeto para "portfolio" e adiciona o componente Header com links de navegação

---
+++ app/components/Header.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+import Image from 'next/image'
+import Link from 'next/link'
+
+export function Header() {
+  const links = [
+    { name: 'HOME', to: '/' },
+    { name: 'CONTATOS', to: '#entre-em-contato' },
+    { name: 'PROJETOS', to: '#meus-projetos' },
+  ]
+
+  return (
+    <header className="w-full text-center bg-gray-900 border-b-2 border-b-gray-700 ">
+      <div className="max-w-[90rem] flex justify-between items-center px-10 mx-auto">
+        <h2 className="sr-only">Header</h2>
+        <Image
+          src={'/logo.jpeg'}
+          alt=""
+          className="h-10 rounded-full"
+          width={40}
+          height={40}
+        />
+        <ul className="font-bold flex justify-center gap-4 items-center py-8 text-cyan-50/75 decoration-current">
+          {links.map((link, index) => (
+            <li
+              key={index}
+              className="hover:text-green-500 uppercase transition-colors text-xs sm:text-base"
+            >
+              <Link href={link.to}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
+
+        <button>aaa</button>
+      </div>
+    </header>
+  )
+}
