25e75a4991915c5c84dfc9509e356304404f99cc
Diego Procopio <diegprocopio98@gmail.com>
2025-04-29 19:37:48 -0300

    feat: atualiza metadados do layout com informações detalhadas e palavras-chave

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,47 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: 'Diego Dev',
-  description: 'Olá, sou Diego Procopio, desenvolvedor full stack júnior com experiência em aplicações web utilizando Vue, React, Node.js e TypeScript.',
+  generator: 'Diego Procopio',
+  applicationName: 'Diego Procopio',
+  referrer: 'origin-when-cross-origin',
+  authors: [{ name: 'Diego Procopio', url: 'https://diegoprocopio.com.br/' }],
+  creator: 'Diego Procopio',
+  publisher: 'Diego Procopio',
+  title: 'Diego Procopio | Desenvolvedor Full Stack Vue, React e Node.js',
+  description:
+    'Diego Procopio é um desenvolvedor full stack especializado em Vue, React, Node.js e TypeScript. Crio aplicações web modernas, rápidas e responsivas. Confira meu portfólio!',
+  keywords: [
+    'desenvolvedor full stack',
+    'programador full stack Vue',
+    'desenvolvedor React e Node.js',
+    'portfólio de programador web',
+    'freelancer full stack',
+    'Diego Procopio programador',
+    'dev Vue React Node',
+    'aplicações web modernas',
+    'web developer brasil',
+    'programador typescript experiente'
+  ],
+  openGraph: {
+    title: 'Diego Procopio | Desenvolvedor Full Stack Vue, React e Node.js',
+    description:
+      'Diego Procopio é um desenvolvedor full stack especializado em Vue, React, Node.js e TypeScript. Crio aplicações web modernas, rápidas e responsivas. Confira meu portfólio!',
+    url: 'https://diegoprocopio.com.br/',
+    siteName: 'Diego Dev',
+    images: [
+      {
+        url: 'https://diegoprocopio.com.br/hand-coding.png',
+        width: 1200,
+        height: 630
+      }
+    ],
+    locale: 'pt_BR',
+    type: 'website'
+  },
+  alternates: {
+    canonical: 'https://diegoprocopio.com.br/'
+  },
+  robots: 'index, follow'
 }
 
 export default function RootLayout({
